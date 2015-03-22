c535103128803a2ce098fb1305322032dc23dd56
Andrew Yale <a.yale9@gmail.com>
2015-03-22 16:39:12 -0400

    remove from socketlog after processed

--- logMonitor.sh
+++ logMonitor.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 # spawns node web server to monitor websocket
-node pushBulletWebsocket.js > socketLog &
+node pushBulletWebsocket.js >> socketLog &
 
 # infite while loop
 # waits until file modified then calls python script
 # spawns script so that multiple instances can run at once
-while inotifywait -e modify socketLog; do
-	python runCommands.py &
+while inotifywait -e modify socketLog
+do
+	while : 
+	do
+		line=$(head -n1 socketLog)
+		if [ -z "$line" ]; then
+			break
+		fi
+		python runCommands.py $line 
+		sed '1d' socketLog > socketLog
+	done
 done
 
