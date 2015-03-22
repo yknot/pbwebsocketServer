713da088b957911aa7e4f2295d33ecf7e6fdcaf1
Shortlife <jmail.yale@gmail.com>
2015-03-22 17:32:08 -0400

    modified runcommand to read and write the datetime stamp for each user in json format

--- runCommands.py
+++ runCommands.py
@@ -1,4 +1,5 @@
 import simplejson
+import json
 import sys
 import requests
 from decimal import *
@@ -13,16 +14,41 @@ from PushBullet import *
 from Pantry import *
 
 
-def latestPush(newPushes):
+def getLastPushDatetime(name):
+  try:
+    # try to find file
+    #since = open('latestPush').readline().strip()
+    with open('latestPush.json') as data_file:    
+        data = json.load(data_file)
+        return data[name]
+
+  except:
+    # if file not there use default
+    return  1424750589.433564
+
+
+def saveLatestPushDatetime(name, newPushes):
     '''update file for latest push timestamp'''
     for p in newPushes['pushes']:
         # write updated latest push
         if 'created' in p:
-            g = open('latestPush', 'w')
-            g.write(repr(p['created']))
-            g.close()
-            return
+           # g = open('latestPush', 'w')
+           # g.write(repr(p['created']))
+           # g.close()
+           # return
 
+            jsonFile = open("latestPush.json", "r")
+            data = json.load(jsonFile)
+            jsonFile.close()
+
+            tmp = data["name"]
+            data["name"] = repr(p['created'])
+  
+            jsonFile = open("latestPush.json", "w+")
+            jsonFile.write(json.dumps(data))
+            jsonFile.close()
+
+            return
 
 def isRelevant(p, pb):
     '''tests if note has all neccessary parts and is sent to server'''
@@ -35,55 +61,53 @@ def isRelevant(p, pb):
         return 1
     else:
         return 0
+
+
+def doStuff(newPushes):
+    # for each command do stuff
+    for p in newPushes['pushes'][:-1]:
+        # if has title, body and target device is server
+        if isRelevant(p, pb):
+            
+            # set to send back to sender
+            if 'source_device_iden' in p:
+                iden = p['source_device_iden']
+            else:
+                iden = ''
+
+            # sets the reciever
+            pb.setIden(iden)
+        
+            # if about pantry
+            if p['title'].lower() == 'pantry':
+                # create pantry
+                pantry = Pantry(pb)
+                # run commands
+                pantry.cmd(p['body'].splitlines())
+                # save the pantry
+                pantry.save()
+
+                pb.deleteNote(p['iden'])
+
+
 # get user name from arguments        
 name = sys.argv[-1]
 
 # new pushbullet object
 pb = PushBullet(name)
-# get new pushes
-try:
-    # try to find file
-    since = open('latestPush').readline().strip()
-except:
-    # if file not there use default
-    since =  1424750589.433564
 
-# get new pushes
+# get last datetime saved for this user name
+since = getLastPushDatetime(name)
+
+# get new pushes for this user name
 rawJSON = pb.getPushes(since)
 newPushes = simplejson.loads(rawJSON)
 
-
-# if there are no newpushes
+# if there are no new pushes
 if 'pushes' not in newPushes:
     sys.exit(0)
 
-# modify latest push file
-latestPush(newPushes)
-
-
-
-# for each command do stuff
-for p in newPushes['pushes'][:-1]:
-    # if has title, body and target device is server
-    if isRelevant(p, pb):
-        
-        # set to send back to sender
-        if 'source_device_iden' in p:
-            iden = p['source_device_iden']
-        else:
-            iden = ''
-
-        # sets the reciever
-        pb.setIden(iden)
-    
-        # if about pantry
-        if p['title'].lower() == 'pantry':
-            # create pantry
-            pantry = Pantry(pb)
-            # run commands
-            pantry.cmd(p['body'].splitlines())
-            # save the pantry
-            pantry.save()
-
-            pb.deleteNote(p['iden'])
+doStuff(newPushes)
             
+# modify latest push file
+saveLatestPushDatetime(name, newPushes)
